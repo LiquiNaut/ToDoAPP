e8c241ab7ada49cab7c5d79c2d6b4b3945c011c0
LiquiNaut <boetfly@gmail.com>
2024-04-30 00:49:58 +0200

    created TodoService for localstorage methods

---
+++ ToDoApp/Services/TodoService.cs
@@ -0,0 +1,25 @@
+﻿using Blazored.LocalStorage;
+using ToDoApp.Models;
+
+namespace ToDoApp.Services;
+
+public class TodoService
+{
+    public readonly ILocalStorageService _LocalStorageService;
+    public const string StorageKey = "todos";
+
+    public TodoService(ILocalStorageService localStorageService)
+    {
+        _LocalStorageService = localStorageService;
+    }
+
+    public async Task SaveTodoList(List<TodoItem> todos)
+    {
+        await _LocalStorageService.SetItemAsync(StorageKey, todos);
+    }
+
+    public async Task<List<TodoItem>> GetTodoItems()
+    {
+        return await _LocalStorageService.GetItemAsync<List<TodoItem>>(StorageKey) ?? new List<TodoItem>();
+    }
+}
